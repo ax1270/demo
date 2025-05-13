839e9adc582565ecf1790945f4a937e22df376a9
kousei1270 <kousei1270@yahoo.co.jp>
2025-05-13 14:26:20 +0900

    test

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,6 +1,6 @@
-// import { testFunction } from './test.js';
+import { testFunction } from './test.js';
 
 export default function decorate(block) {
-  block.setAttribute('id', `aaaa`);
-  // testFunction(block);
+  // block.setAttribute('id', `aaaa`);
+  testFunction(block);
 }
--- blocks/banner/test.js
+++ blocks/banner/test.js
@@ -1,3 +1,3 @@
 function testFunction(block) {
-    block.setAttribute('id2', `test`);
+    block.setAttribute('id', `bbbb`);
 }
