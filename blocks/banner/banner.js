370c5c6e631798708ee29f03f01cc2b7c74af980
kousei1270 <kousei1270@yahoo.co.jp>
2025-05-13 14:56:48 +0900

    test

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -2,5 +2,6 @@
 import { testFunction } from './test.js';
 
 export default function decorate(block) {
-  block.setAttribute('id', `aaaa`);
+  // block.setAttribute('id', `aaaa`);
+  testFunction(block);
 }
