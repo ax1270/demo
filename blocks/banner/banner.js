c7b6d108f86652c0398a2d467573d0fbbef9d911
kousei1270 <kousei1270@yahoo.co.jp>
2025-05-13 15:15:59 +0900

    test

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,7 +1,7 @@
 import { testFunction } from './test.js';
-import { decorate } from '../carousel/carousel.js';
+// import { decorate } from '../carousel/carousel.js';
 
 export default function decorate(block) {
   testFunction(block);
-  decorate(block);
+  // decorate(block);
 }
