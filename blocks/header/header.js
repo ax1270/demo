bc50676adffa8a05b7b0785a47a21d3e55ce1e4c
kousei1270 <kousei1270@yahoo.co.jp>
2025-05-14 15:09:14 +0900

    test

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -235,6 +235,6 @@ export default async function decorate(block) {
 
   // breadcrumbs
   if (getMetadata('breadcrumbs').toLowerCase() === 'true') {
-    navWrapper.append(await buildBreadcrumbs());
+    navWrapper.append(buildBreadcrumbs());
   }
 }
