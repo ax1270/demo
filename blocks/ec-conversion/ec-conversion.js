c654d8384edba692c0fd2e375153e550b5cc258f
kousei1270 <kousei1270@yahoo.co.jp>
2025-06-12 16:57:25 +0900

    test

--- blocks/ec-conversion/ec-conversion.js
+++ blocks/ec-conversion/ec-conversion.js
@@ -1,8 +1,7 @@
 import { productInfo } from 'product-info.json';
 
 export default function decorate(block) {
-//   block.classList.add(`ec-conversion`);
-//   const slides = block.querySelectorAll('.ec-conversion');
-//   const productId = getMetadata('productId');
-//   block.append(document.createTextNode('製品情報ID'), document.createElement('div'), productId);
+  const tag = block.querySelectorAll('.ec-conversion');
+  const productId = getMetadata('productId');
+  tag.append(document.createTextNode('製品情報ID'), document.createElement('div'), productId);
 }
