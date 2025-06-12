f80c9ca3f279afc69837805f7d0a6912aadbab58
kousei1270 <kousei1270@yahoo.co.jp>
2025-06-12 17:08:33 +0900

    test

--- blocks/ec-conversion/ec-conversion.js
+++ blocks/ec-conversion/ec-conversion.js
@@ -1,7 +1,7 @@
 import { productInfo } from 'product-info.json';
 
 export default function decorate(block) {
-  const productId = getMetadata('productid ');
+  const productId = getMetadata('productid');
   block.setAttribute('id', productId);
 //   tag.append(document.createTextNode('製品情報ID'), document.createElement('div'), productId);
 }
