dea2b5b4c9fe9f3e802ebac14ac5349fbe9a7dbf
kousei1270 <kousei1270@yahoo.co.jp>
2025-06-12 17:15:35 +0900

    test

--- blocks/ecconversion/ecconversion.js
+++ blocks/ecconversion/ecconversion.js
@@ -1,6 +1,5 @@
 import { productInfo } from 'product-info.json';
 
 export default function decorate(block) {
-//   const productId = getMetadata('productid');
-  block.setAttribute('id', 'test');
+  block.setAttribute('id', `test`);
 }
