4d6ab8e92c59a0b1edb4c8eb3c4eefaf14ff9d62
kousei1270 <kousei1270@yahoo.co.jp>
2025-06-13 10:51:12 +0900

    test

--- blocks/ec-conversion/ec-conversion.js
+++ blocks/ec-conversion/ec-conversion.js
@@ -1,14 +1,18 @@
 import { getMetadata } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const productId = getMetadata('ProductId');
+  // 製品情報ID
+  const productId = getMetadata('Productid');
+
+  // 製品情報リスト
+  const productList = '/product-info.json';
 
   block.setAttribute('id', `testId`);
 
   if (getMetadata('productid') === "1111") {
     const container = document.createElement('div');
     container.classList.add('testClass');
-    container.append(`bbbbb`);
+    container.append(`cccc`);
     block.prepend(container);
   }
 }
